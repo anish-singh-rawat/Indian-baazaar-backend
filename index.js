8c64fd67c8f8d95ac5d7d731689d160e77387a30
anish-singh-rawat <www.computernetwork@gmail.com>
2025-11-07 06:10:19 +0530

    fix backend : remove the logo related api's and all the logic

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ import bannerV1Router from './route/bannerV1.route.js';
 import bannerList2Router from './route/bannerList2.route.js';
 import blogRouter from './route/blog.route.js';
 import orderRouter from './route/order.route.js';
-import logoRouter from './route/logo.route.js';
 
 const app = express();
 app.use(cors({
@@ -68,7 +67,6 @@ app.use("/api/bannerV1",bannerV1Router)
 app.use("/api/bannerList2",bannerList2Router)
 app.use("/api/blog",blogRouter)
 app.use("/api/order",orderRouter)
-app.use("/api/logo",logoRouter)
 
 app.use((err, req, res, next) => {
     console.error('Unhandled error:', err);
